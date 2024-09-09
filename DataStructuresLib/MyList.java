c2ea2c8dd5c35706fc8d29f6c50743b26fcc83c8
Sam Goree <sgoree@stonehill.edu>
2024-09-09 11:24:13 -0400

    adds starter code for myarraylist

---
+++ DataStructuresLib/MyList.java
@@ -0,0 +1,13 @@
+package DataStructuresLib;
+
+abstract class MyList<T> {
+    abstract void add(T value);
+    abstract void add(T value, int position);
+    abstract T remove(int position);
+    abstract T get(int position);
+    abstract T set(int position, T newValue);
+    abstract int size();
+    abstract boolean contains(T value);
+    abstract boolean isEmpty();
+    abstract void clear();
+}
